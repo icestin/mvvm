9b2ee7b0b3f1385c9d01035eb6dae8886c28e1c9
icestin <ning231@126.com>
2018-04-08 16:30:17 +0800

    utils

---
+++ React3/app/component/demo/demo2/TodoList.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import List from './List';
+// import '../../../../public/css/todoList.pcss'
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {
+                 id: 1,
+                 title: '项目1',
+                 status: 1,
+                },
+                {
+                    id: 2,
+                    title: '项目2',
+                    status: 1,
+                }
+        ]
+        };
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleItemDel = this.handleItemDel.bind(this);
+        this.handleItemRecovery = this.handleItemRecovery.bind(this);
+        this.handleItemEdit = this.handleItemEdit.bind(this);
+    }
+    //新增
+    handleAdd() {
+        let item = this.refs['todoInput'].value;
+        if (item) {
+            let list = this.state.list;
+            list.push({
+                id:list.length + 1,
+                title: item,
+                status: 1
+            })
+            this.setState({
+                list: list
+            },()=>console.log("当前集合", this.state.list))
+        } else {
+            alert('不能为空')
+        }
+    }
+    handleItemEdit (id, type) {
+        let list = this.state.list;
+        list.find(data=>data.id===id).status = type;
+        this.setState({
+            list: list
+        })
+    }
+    // 删除事件
+    handleItemDel (id) {
+        let list = this.state.list;
+        list.find(data=>data.id===id).status = 0;
+        this.setState({
+            list: list
+        })
+    }
+    // 恢复
+    handleItemRecovery (id) {
+        let list = this.state.list;
+        list.find(data => data.id ===id).status = 1;
+        this.setState({list: list});
+    }
+
+    componentDidMount () {
+
+    }
+
+    render() {
+        let list = this.state.list;
+        return (
+            <div className="todoList">
+                <input type="text" ref='todoInput'/> 
+                <button onClick={this.handleAdd.bind(this)}> 添加</button>
+
+                <div className = 'cont'>
+                    <div className="box"> 全部
+                    <List list = {list} handleItemEdit = {this.handleItemEdit}  type = {0}/>
+                    </div>
+                </div>
+
+                <div className = 'cont'>
+                    <div className="box"> 未删除
+                    <List list = {list} handleItemEdit = {this.handleItemEdit} type = {1}/>
+                    </div>
+                </div>
+
+                <div className = 'cont'>
+                    <div className="box"> 已删除
+                    <List list = {list} handleItemEdit = {this.handleItemEdit} type = {2}/>
+                    </div>
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default TodoList;
---
+++ React3/app/component/shop/Index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// import '../../../public/css/shop.pcss'
+import utils from '../../../public/js/utils';
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {id:1, title:'列表1'},
+                {id:2, title:'列表2'},
+                {id:3, title:'列表3'},
+                {id:4, title:'列表4'},
+                {id:5, title:'列表5'},
+                {id:6, title:'列表6'},
+            ]
+        }
+    }
+    handleItemDel (id) {
+        console.log("数据项ID",id);
+        let list = this.state.list;
+        list.splice(list.findIndex(data=>data.id===id),1);
+        this.setState({list: list})
+    }
+    render() {
+        let { list } = this.state;
+        return (
+            <div className="content">
+                 <div>获取url中的参数name的值: {utils.urlParam('name')}</div>
+                 <div>18765929496:这{utils.isMobile('18765929496') ? "是":"不是" }手机号</div>
+                 <div>12111111111:这{utils.isMobile('12111111111') ? "是":"不是" }手机号</div>
+             {
+                list.map((data, index)=>{
+                     return (<li key={data.id}> {data.title} 
+                     {/* <button onClick = {this.handleItemDel.bind(this,data.id)}>删除</button></li>) */}
+                     <button onClick = {()=>this.handleItemDel(data.id)}>删除</button></li>)
+                 })
+             }
+            </div>
+        )
+    }
+}
+
+export default Index;
