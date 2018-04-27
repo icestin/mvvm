a57d8e6d90e3885e9e9fdf543e12ce2975130e42
icestin <ning231@126.com>
2018-04-27 19:47:25 +0800

    reflux request

--- React7/app/component/demo/reflux/Index.jsx
+++ React7/app/component/demo/reflux/Index.jsx
@@ -4,6 +4,7 @@ import ReFlux1 from './reflux1/Index';
 import ReFlux2 from './reflux2/Index';
 import ReFlux3 from './reflux3/Index';
 import ReFlux4 from './reflux4/Index';
+import ReFlux5 from './reflux5/Index';
 
 const Index = ({match}) => 
       <HashRouter >
@@ -13,6 +14,7 @@ const Index = ({match}) =>
             <NavLink to="/ReFlux/ReFlux2" activeClassName="selected" > ReFlux2 </NavLink>
             <NavLink to="/ReFlux/ReFlux3" activeClassName="selected" > ReFlux3 </NavLink>
             <NavLink to="/ReFlux/ReFlux4" activeClassName="selected" > ReFlux4 </NavLink>
+            <NavLink to="/ReFlux/ReFlux5" activeClassName="selected" > ReFlux5 </NavLink>
          </div>
          <Route exact path={`${match.url}`}
            render={() => (<Redirect to={`${match.url}/ReFlux1`}/>)}
@@ -21,6 +23,7 @@ const Index = ({match}) =>
          <Route path={`${match.url}/ReFlux2`}  component={ReFlux2} />
          <Route path={`${match.url}/ReFlux3`}  component={ReFlux3} />
          <Route path={`${match.url}/ReFlux4`}  component={ReFlux4} />
+         <Route path={`${match.url}/ReFlux5`}  component={ReFlux5} />
         </div>
       </HashRouter>
 
